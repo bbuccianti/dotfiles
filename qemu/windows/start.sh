a01d47291d23885a6d734f693f8fcf2e6d14a520
Benjamin Buccianti <benjamin@buccianti.dev>
2020-01-14 18:57:57 -0300

    qemu/windows: fix for some issues with mouse

--- qemu/windows/start.sh
+++ qemu/windows/start.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+export SDL_VIDEO_X11_DGAMOUSE=0
+
 qemu-system-x86_64 \
     -enable-kvm \
     -m 3072 \
+    -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time \
     -net user -net nic \
-    -drive file=windows.qcow2,media=disk,if=virtio \
+    -drive file=windows.qcow2,media=disk,if=virtio,aio=native,cache.direct=on \
     -drive file=virtio.iso,media=cdrom \
     #-drive file=windows.iso,media=cdrom \
+    -usbdevice tablet \
     -vga virtio \
     -display sdl,gl=on
