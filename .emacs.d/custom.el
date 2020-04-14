a6bf9093d64c07ba00699cbef44faa4b9245085e
Benjamin Buccianti <benjamin@buccianti.dev>
2020-04-14 15:50:51 -0300

    emacs: bold parents with rainbow delimiters

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,11 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("2fca995d6859455e338f4b094c07ba924d05dc3d548a51c4fcca6e478c24663e" default)))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(rainbow-delimiters-base-face ((t (:inherit nil))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#56B6C2" :weight bold))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "#D19A66" :weight bold))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "#61AFEF" :weight bold))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "#C678DD" :weight bold))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "#98C379" :weight bold))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "#BE5046" :weight bold))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "#56B6C2" :weight bold))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "#E5C07B" :weight bold))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "#61AFEF" :weight bold)))))
