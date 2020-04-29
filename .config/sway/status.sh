29b1b346de8a32f69911632a32eee8e8a652597f
Benjamin Buccianti <benjamin@buccianti.dev>
2020-04-29 14:35:06 -0300

    sway: fix mail notifications

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -6,6 +6,8 @@ MAILS=$(mu find flag:unread | wc -l)
 if test "$MAILS" != "0"
 then
     /bin/notify-send.sh --replace-file=/tmp/email "$MAILS new emails"
+else
+    echo 25 > /tmp/email
 fi
 
 echo "$MAILS | $DATE"
