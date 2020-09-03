e73c0a9433fba85b9228fc1f814f094803d5792f
Benjamin Buccianti <benjamin@buccianti.dev>
2020-09-03 20:49:34 -0300

    sway: show song

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -2,10 +2,11 @@
 
 DATE=$(date +%e/%m\ %H:%M)
 MAILS=$(/bin/notmuch count tag:unread)
+SONG=$(mpc status | head -1 | xargs -0 basename)
 
 if test "$MAILS" != "0"
 then
     notify-send --icon=mail-unread -t 30000 "$MAILS new emails"
 fi
 
-echo "$MAILS | $DATE"
+echo "$SONG | $MAILS | $DATE"
