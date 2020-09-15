97cd1b305f31db92eef4ed5a73cfebd1b02d43c4
Benjamin Buccianti <benjamin@buccianti.dev>
2020-09-15 12:26:10 -0300

    emacs: rainbow delimiters now bold

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -10,13 +10,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(rainbow-delimiters-base-face ((t (:inherit nil))))
- '(rainbow-delimiters-depth-1-face ((t (:foreground "#56B6C2" :weight bold))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "#D19A66" :weight bold))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "#61AFEF" :weight bold))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "#C678DD" :weight bold))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "#98C379" :weight bold))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "#BE5046" :weight bold))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "#56B6C2" :weight bold))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "#E5C07B" :weight bold))))
- '(rainbow-delimiters-depth-9-face ((t (:foreground "#61AFEF" :weight bold)))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#145c33" :weight bold))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "#5317ac" :weight bold))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "#005a5f" :weight bold))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "#863927" :weight bold))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "#0000bb" :weight bold))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "#315b00" :weight bold))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "#8f0075" :weight bold))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "#30517f" :weight bold))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "#70480f" :weight bold)))))
