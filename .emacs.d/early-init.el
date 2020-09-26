7fa1e9d13b805d9bdd4745f9e0f91707500f1400
Benjamin Buccianti <benjamin@buccianti.dev>
2020-09-26 19:32:44 -0300

    emacs: early-init set variables default values

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,20 +1,21 @@
 (defvar old--file-name-handler-alist file-name-handler-alist)
 
-(setq gc-cons-threshold (* 100 1024 1024) ;; 100mb
-      gc-cons-percentage 0.6
-      package-enable-at-startup nil
-      menu-bar-mode nil
-      tool-bar-mode nil
-      column-number-mode t
-      frame-inhibit-implied-resize t
-      site-run-file nil
-      initial-scratch-message nil
-      inhibit-startup-screen t
-      inhibit-default-init t
-      inhibit-startup-message t
-      bidi-display-reordering nil
-      create-lockfiles nil
-      file-name-handler-alist nil)
+(setq-default gc-cons-threshold (* 100 1024 1024) ;; 100mb
+	      gc-cons-percentage 0.6
+	      package-enable-at-startup nil
+	      menu-bar-mode nil
+	      tool-bar-mode nil
+	      column-number-mode t
+	      frame-inhibit-implied-resize t
+	      site-run-file nil
+	      initial-scratch-message nil
+	      inhibit-startup-screen t
+	      inhibit-default-init t
+	      inhibit-startup-message t
+	      bidi-display-reordering nil
+	      bidi-paragraph-direction 'left-to-right
+	      create-lockfiles nil
+	      file-name-handler-alist nil)
 
 (advice-add #'package--ensure-init-file :override #'ignore)
 (advice-add #'x-apply-session-resources :override #'ignore)
