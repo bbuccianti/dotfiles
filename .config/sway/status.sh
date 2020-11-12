c72583cd705ed5e923f3226374773e5e1cbb379f
Benjamin Buccianti <benjamin@buccianti.dev>
2020-11-12 19:39:36 -0300

    nc hack to get current song from mpd

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 DATE=$(date +%e/%m\ %H:%M)
+ARTIST=$(echo -e "currentsong\nclose" | nc localhost 6600 | awk -F: '/Artist/ { print $2; }')
+SONG=$(echo -e "currentsong\nclose" | nc localhost 6600 | awk -F: '/Title/ { print $2; }')
 
-echo "$DATE"
+echo "$ARTIST | $SONG | $DATE"
