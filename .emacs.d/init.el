f5d2a8ae94b1079fcec4bcd28a7a5ae8401934be
Benjamin Buccianti <benjamin@buccianti.dev>
2021-03-14 15:40:05 -0300

    move org agenda to his own bin

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -272,60 +272,6 @@
   :config
   (setq magit-repository-directories '(("~/work" . 2) ("~/src" . 3))))
 
-(use-package org
-  :mode (("\\.org\\'" . org-mode))
-  :bind (:map ctl-z-map
-	      ("a" . org-agenda)
-	      ("c" . org-capture))
-  :config
-  (setq org-startup-indented t
-	org-startup-truncated nil
-	org-modules '(ol-info ol-mhe ol-rmail)
-	org-hide-leading-stars t
-	org-latex-toc-command "\\tableofcontents \\clearpage"
-	org-export-async-init-file "~/.emacs.d/org-init.el"
-	org-src-preserve-indentation t
-	org-default-notes-file "/home/bbuccianti/org/notes.org"
-	org-refile-use-outline-path t
-	org-outline-path-complete-in-steps nil
-	org-completion-use-ido nil
-	org-log-done "note"
-	org-tags-column -60
-	org-fast-tag-selection-single-key 'expert
-	org-blank-before-new-entry '((heading . auto) (plain-list-item . auto))
-	org-refile-targets '((nil :maxlevel . 9)
-			     (org-agenda-files :maxlevel . 9))
-	org-capture-templates '(("t" "Todo"
-				 entry (file+headline "~/org/projects.org"
-						      "Inbox")
-				 "* TODO %?\n:PROPERTIES:\n:CREATED: %U\n:END:")
-				("n" "Note"
-				 entry (file "~/org/notes.org")
-				 "* %?\n:PROPERTIES:\n:CREATED: %U\n:END:"))
-	org-todo-keywords '((sequence "PROJECT(p)" "TODO(t)" "|"
-				      "DONE(d)" "CANCELLED(c)"))
-	org-directory "/home/bbuccianti/notes/"))
-
-(use-package org-agenda
-  :config
-  (setq org-agenda-files '("/home/bbuccianti/org/")
-	org-agenda-skip-deadline-if-done t
-	org-agenda-skip-scheduled-if-deadline-is-shown t
-	org-agenda-skip-scheduled-if-done t
-	org-agenda-skip-unavailable-files t
-	org-agenda-window-setup 'current-window
-	org-agenda-start-on-weekday nil
-	org-agenda-compact-blocks nil
-	org-agenda-prefix-format '((agenda . " %i %-12:c%?-12t% s")
-				   (todo . "%l" )
-				   (tags . "%l")
-				   (search . " %i %-12:c"))))
-
-(use-package notdeft
-  :straight (:host github :repo "hasu/notdeft" :files ("*.el" "xapian"))
-  :config (setq notdeft-directories '("~/notes")
-		notdeft-extension "org"))
-
 (use-package ediff
   :config
   (setq ediff-window-setup-function 'ediff-setup-windows-plain
